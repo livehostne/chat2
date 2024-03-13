4fb46481adb41cbc0b294b9a98a6e1b6cf67bea4
livehostne <163356470+livehostne@users.noreply.github.com>
2024-03-13 19:16:23 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("ws://chat2-b6l6.onrender.com")
     websocket.onmessage = processMessage
 }
 
